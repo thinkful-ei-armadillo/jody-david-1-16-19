c1113d4865127b61d3a7d1d564faf820acad2871
Jodster1010 <jody.cook@mac.com>
2019-01-16 08:35:24 -0800

    add drill 2

--- drills.js
+++ drills.js
@@ -1,13 +1,22 @@
 'use strict'
+debugger;
 
-function createMyObject(){
-  return {
-    foo: 'bar',
-    answerToUniverse: 42,
-    'olly olly': 'oxen free',
-    sayHello: function(){
-      return 'hello';
-    }
-  };
+// function createMyObject(){
+//   return {
+//     foo: 'bar',
+//     answerToUniverse: 42,
+//     'olly olly': 'oxen free',
+//     sayHello: function(){
+//       return 'hello';
+//     }
+//   };
+// }
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
 }
-
+console.log(updateObject());
